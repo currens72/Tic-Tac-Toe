1f43b8b48d81d9fe3e5e57962e6234266582475d
Thomas <trglass@iastate.edu>
2022-10-27 13:23:07 -0500

    Testing all buttons and drawing O's and X's

--- ticTacToe.py
+++ ticTacToe.py
@@ -7,23 +7,48 @@ pygame.font.init()
 screen = pygame.display.set_mode([600, 600])
 running = True
 
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
 while running:
+
+    def drawCircle(coordinates):
+        pygame.draw.circle(screen, black, coordinates, 75, 2)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
         if event.type == pygame.MOUSEBUTTONDOWN:
+            if mouse[0] < 200 and mouse[0] > 000 and mouse[1] > 000 and mouse[1] < 200:
+                print("1")
+            if mouse[0] < 400 and mouse[0] > 200 and mouse[1] > 000 and mouse[1] < 200:
+                print("2")
+            if mouse[0] < 600 and mouse[0] > 400 and mouse[1] > 000 and mouse[1] < 200:
+                print("3")
+            if mouse[0] < 200 and mouse[0] > 000 and mouse[1] > 200 and mouse[1] < 400:
+                print("4")
             if mouse[0] < 400 and mouse[0] > 200 and mouse[1] > 200 and mouse[1] < 400:
-                print("Clicked")
+                print("5")
+            if mouse[0] < 600 and mouse[0] > 400 and mouse[1] > 200 and mouse[1] < 400:
+                print("6")
+            if mouse[0] < 200 and mouse[0] > 000 and mouse[1] > 400 and mouse[1] < 600:
+                print("7")
+            if mouse[0] < 400 and mouse[0] > 200 and mouse[1] > 400 and mouse[1] < 600:
+                print("8")
+            if mouse[0] < 600 and mouse[0] > 400 and mouse[1] > 400 and mouse[1] < 600:
+                print("9")
+                drawCircle((500, 500))
 
     screen.fill((255, 255, 255))
     mouse = pygame.mouse.get_pos()
 
-    pygame.draw.line(screen, (0, 0, 0), (0, 200), (600, 200), 1)
-    pygame.draw.line(screen, (0, 0, 0), (0, 400), (600, 400), 1)
-    pygame.draw.line(screen, (0, 0, 0), (200, 0), (200, 600), 1)
-    pygame.draw.line(screen, (0, 0, 0), (400, 0), (400, 600), 1)
-    pygame.draw.rect(screen, (250, 0, 0), pygame.Rect(200, 200, 200, 200), 1)
+    pygame.draw.line(screen, black, (0, 200), (600, 200), 1)
+    pygame.draw.line(screen, black, (0, 400), (600, 400), 1)
+    pygame.draw.line(screen, black, (200, 0), (200, 600), 1)
+    pygame.draw.line(screen, black, (400, 0), (400, 600), 1)
 
     
     pygame.display.flip()
